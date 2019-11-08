c0f317237504bfc6958253f8e0794e6a3d238eae
sam-eng <sme388@nyu.edu>
2019-11-08 16:20:05 -0500

    add postTweet function

---
+++ Twitter/TweetsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TweetsViewController.swift
+//  Twitter
+//
+//  Created by Samantha Eng on 11/8/19.
+//  Copyright © 2019 Samantha Eng. All rights reserved.
+//
+
+import UIKit
+
+class TweetsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Button Functions
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    @IBAction func tweet(_ sender: Any) {
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
